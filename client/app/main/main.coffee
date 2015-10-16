1db243b5e95885e4567284e99c17e83af0c99985
nhannhan159 <tanquoctien@gmail.com>
2015-10-16 13:08:24 +0700

    - Init T4Logs client.

---
+++ client/app/main/main.coffee
@@ -0,0 +1,9 @@
+'use strict'
+
+angular.module 't4LogsApp'
+.config ($stateProvider) ->
+  $stateProvider
+  .state 'main',
+    url: '/'
+    templateUrl: 'app/main/main.html'
+    controller: 'MainCtrl'
---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/t4logs-dev'
+  },
+
+  seedDB: true
+};
